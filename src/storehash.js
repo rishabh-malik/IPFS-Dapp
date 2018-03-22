34865364a6112ad965f600809470292984d12ae1
rishabh-malik <rishabhmalik249@gmail.com>
2018-03-22 18:44:05 +0530

    storehash.js

---
+++ src/storehash.js
@@ -0,0 +1,36 @@
+import web3 from './web3';
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0x531864496ab69d230af9f4fc738655f9f05e0e79';
+//use the ABI from your contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
